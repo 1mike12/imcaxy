2b4d2387b40dcb09694401e2e38bd39734dbf456
Bartłomiej Banach <thebartekbanach.dev@gmail.com>
2021-10-28 14:35:40 +0200

    fix: add missing GetOrCreateStream DataHub interface method

--- pkg/hub/interface.go
+++ pkg/hub/interface.go
@@ -24,6 +24,11 @@ type (
 		StartMonitors(ctx context.Context)
 		CreateStream(streamID string) (DataStreamInput, error)
 		GetStreamOutput(streamID string) (DataStreamOutput, error)
+
+		// If DataStreamInput is nil, it means that stream is already writing in other
+		// goroutine, so we can just read the data that is sent through stream without
+		// need to double the work and manually download and write it.
+		GetOrCreateStream(streamID string) (DataStreamOutput, DataStreamInput, error)
 	}
 )
 
